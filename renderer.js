0ca214ae2874be237d571905e97e24e933cbbe90
ooi <bitbucket.ooi.hiroki@gmail.com>
2019-01-05 09:05:16 +0900

    no message

--- renderer.js
+++ renderer.js
@@ -2,12 +2,23 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-const ffi = require('ffi')
+// const ffi = require('ffi')
+const ffi = require('./node-ffi')
+const fs = require('fs')
+const path = require('path')
+
+const DDL = 'libfactorial.dll'
+
+let ddlPath = path.join(process.cwd(), DDL)
+if (!fs.existsSync(ddlPath)) {
+    ddlPath = path.join(process.cwd(), 'resources', 'app', DDL)
+}
+
+console.log(`path -> ${ddlPath}`)
 
 var libfactorial = ffi.Library('libfactorial.dll', {
   'factorial': [ 'uint64', [ 'int' ] ]
 })
-
 
 var output = libfactorial.factorial(parseInt("35"))
 
